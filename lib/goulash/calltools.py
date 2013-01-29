855bc8bc9ec28ec06e7a88580c234a005e1b3a7e
mvr <matthewvonrocketstein@gmail-dot-com>
2013-01-28 23:23:20 -0500

    more cowbell

---
+++ lib/goulash/calltools.py
@@ -0,0 +1,14 @@
+""" goulash.calltools
+"""
+
+class callchain(object):
+    # TODO: user itertools
+    def __init__(self, chain):
+        self.chain = chain
+
+    def __str__(self):
+        return 'call-chain: '+str([x.__name__ for x in self.chain])
+
+    def __call__(self, *args, **kargs):
+        results = [x(*args, **kargs) for x in self.chain]
+        return results[-1]
