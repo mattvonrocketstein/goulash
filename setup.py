cc5f0396d2e1e443f1992c7c295f35eb401b5366
mvr <matthewvonrocketstein@gmail-dot-com>
2014-07-02 07:16:03 -0400

    trying to pypi

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@
 """
 from setuptools import setup, find_packages
 setup(
-    name        = 'goulash',
-    version     = '.1',
-    description = 'object namespace partitioner',
-    author      = 'mattvonrocketstein, in the gmails',
-    url         = 'one of these days',
-    package_dir = {'': 'lib'},
-    packages    = find_packages('lib'),
+    name         = 'goulash',
+    version      = '0.1',
+    description  = 'toolbox, random shared stuff from my other projects',
+    author       = 'mattvonrocketstein, in the gmails',
+    url          = 'one of these days',
+    download_url = 'https://github.com/mattvonrocketstein/goulash/tarball/0.1',
+    package_dir  = {'': 'lib'},
+    packages     = find_packages('lib'),
+    keywords     = ['goulash']
     )
