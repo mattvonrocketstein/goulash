f90588e3206e56cacbcf1fd6c7e46f846eb06368
mvr <matthewvonrocketstein@gmail-dot-com>
2014-11-10 08:24:48 +0000

    add summarize_fpath util

--- goulash/util.py
+++ goulash/util.py
@@ -1,6 +1,15 @@
 """ goulash.util
 """
+import os
 import time, uuid
+
+def summarize_fpath(fpath):
+    """ truncates a filepath to be more suitable for display.
+        every instance of $HOME is replaced with ~
+    """
+    home = os.environ.get('HOME', None)
+    if home:
+        return fpath.replace(home, '~')
 
 def uniq(use_time=False):
     """ """
