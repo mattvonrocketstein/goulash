efe62619535d79fb67f8964cbc49a908e93ed552
mvr <matthewvonrocketstein@gmail-dot-com>
2014-11-15 01:13:43 +0000

    add 'which' command

--- goulash/_os.py
+++ goulash/_os.py
@@ -1,7 +1,11 @@
-""" goulash._os
+""" goulash.util._os
 """
+import os
+
+def which(name):
+    return os.popen('which '+name).readlines()[0].strip()
+
 def get_mounts_by_type(mtype):
-    import os
     tmp = os.popen('mount -l -t {0}'.format(mtype))
     tmp = tmp.readlines()
     tmp = [x.strip() for x in tmp if x.strip()]
