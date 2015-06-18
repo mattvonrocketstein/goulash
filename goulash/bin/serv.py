9fc24e4b67783a6dfe2e7d7a2c8ffb44ea19325f
mvr <matthewvonrocketstein@gmail-dot-com>
2015-06-18 18:54:24 +0000

    simple threaded directory indexing http server

--- goulash/bin/serv.py
+++ goulash/bin/serv.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 """ goulash.bin.serv
-
 """
+# SOURCE:
+#  http://stackoverflow.com/questions/14088294/multithreaded-web-server-in-python
 import os, sys
-import SocketServer
-import BaseHTTPServer
+from SocketServer import ThreadingMixIn
+from BaseHTTPServer import HTTPServer
 import SimpleHTTPServer
 
 from argparse import ArgumentParser
@@ -28,16 +29,15 @@ def get_parser():
         help=("directory to serve files from"))
     return parser
 
-class ThreadingSimpleServer(
-    SocketServer.ThreadingMixIn,
-    BaseHTTPServer.HTTPServer):
+class ThreadingSimpleServer(ThreadingMixIn, HTTPServer):
     pass
 
 def main(args):
     port = int(args.port) if args.port else 8000
     _dir = args.dir if args.dir else os.getcwd()
     if not os.path.exists(_dir):
-        raise SystemExit("cannot serve nonexistent directory: {0}".format(_dir))
+        err = "cannot serve nonexistent directory: {0}".format(_dir)
+        raise SystemExit(err)
     os.chdir(_dir)
     msg = "starting file server. port is {0}, directory is {1}"
     msg = msg.format(port, _dir)
@@ -56,7 +56,7 @@ def entry(dir=None):
     parser = get_parser()
     args = parser.parse_args()
     if dir:
-        args.dir=dir
+        args.dir = dir
     if args.version:
         print version.__version__
         raise SystemExit()
